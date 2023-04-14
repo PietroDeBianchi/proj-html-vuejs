421ca6a9f3df7f88610b3753ec147d9029c4f14b
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-04-14 15:53:00 +0200

    footer done! finish some extra styles

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -16,16 +16,24 @@ export default {
                     <h2>About</h2>
                     <div class="line"></div>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quibusdam rerum amet architecto,
-                        aperiam enim at quas, fuga, quaerat minus nihil iusto neque!</p>
+                        aperiam enim at fuga!</p>
                     <div class="icons">
-                        <i class="fa-brands fa-linkedin"></i>
-                        <i class="fa-brands fa-facebook"></i>
-                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-linkedin-in rounded"></i>
+                        <i class="fa-brands fa-facebook-f square"></i>
+                        <i class="fa-brands fa-twitter rounded"></i>
                     </div>
                 </div>
                 <div class="column">
                     <h2>Twitter</h2>
                     <div class="line"></div>
+                    <div class="twitterBox"><i class="fa-brands fa-twitter"></i>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nemo architecto tempora reiciendis
+                            dolore nulla natus aspernatur</p>
+                    </div>
+                    <div class="twitterBox"><i class="fa-brands fa-twitter"></i>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nemo architecto tempora reiciendis
+                            dolore nulla natus aspernatur</p>
+                    </div>
                 </div>
                 <div class="column">
                     <h2>Important Links</h2>
@@ -41,9 +49,14 @@ export default {
                 <div class="column">
                     <h2>Contact Me</h2>
                     <div class="line"></div>
-                    <div><i class="fa-solid fa-location-dot"></i><span>Via TaldeiTali, Speranza, 911</span></div>
-                    <div><i class="fa-solid fa-location-dot"></i><span>Via TaldeiTali, Speranza, 911</span></div>
-                    <div><i class="fa-solid fa-location-dot"></i><span>Via TaldeiTali, Speranza, 911</span></div>
+                    <div class="contact"><i class="fa-solid fa-location-dot"></i><span> Via TaldeiTali, Speranza,
+                            911</span></div>
+                    <div class="contact"><i class="fa-solid fa-phone"></i><span> (+351) 333.179.3459</span></div>
+                    <div class="contact"><i class="fa-regular fa-envelope"></i><span> everlead@studio.com</span></div>
+                    <div class="footerform">
+                        <label for="name"></label>
+                        <input type="text" id="name" name="name" placeholder=" Your name" required>
+                    </div>
                 </div>
             </div>
         </div>
@@ -80,9 +93,16 @@ section {
     }
 
     a,
-    p {
+    p,
+    span {
         font-size: 14px;
         color: $gray-third-color;
+        padding: 6px 0;
+        display: inline-block;
+
+        &:hover {
+            color: $white-first-color;
+        }
     }
 
     .line {
@@ -97,10 +117,56 @@ section {
     margin-top: 12px;
 
     i {
+        cursor: pointer;
         margin-right: 8px;
         background-color: $white-first-color;
-        padding: 3px;
         border-radius: 50%;
+
+        &:hover {
+            background-color: $orange-first-color;
+            color: $white-first-color;
+        }
+    }
+
+    .square {
+        padding: 3px 6px;
+    }
+
+    .rounded {
+        padding: 3px;
+    }
+}
+
+.twitterBox {
+    display: flex;
+    align-items: flex-start;
+
+    i {
+        padding: 8px;
+        color: $orange-first-color;
+    }
+}
+
+.contact {
+
+    span {
+        margin-left: 8px;
+    }
+
+    i {
+        color: $orange-first-color;
+    }
+}
+
+.footerform {
+    margin-top: 8px;
+
+    input {
+        background-color: transparent;
+        border: none;
+        padding-bottom: 6px;
+        border-bottom: 1px solid lightgray;
+        width: 100%;
     }
 }
 </style>
